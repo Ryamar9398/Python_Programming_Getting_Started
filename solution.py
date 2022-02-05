b44606d7b170d90f91c1b5dbfcb592ba6d9d655f
Ryamar9398 <98908228+Ryamar9398@users.noreply.github.com>
2022-02-05 11:26:23 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -27,10 +27,10 @@ def welcome_assignment_answers(question):
 # Complete all the questions.
 
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
     #use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
-    print(welcome_assignment_answers(debug_question))
+    #debug_question = "Are encoding and encryption the same? - Yes/No"
+    #print(welcome_assignment_answers(debug_question))
 
 ###Questions:
 ###"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?"
