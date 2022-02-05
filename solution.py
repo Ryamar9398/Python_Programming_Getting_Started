572fde2ec1edb90725f326b4eaa20df9062e4ad4
Ryamar9398 <98908228+Ryamar9398@users.noreply.github.com>
2022-02-05 11:15:41 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -23,7 +23,7 @@ def welcome_assignment_answers(question):
         answer = int("5")
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer = int("4")
-    return(answer)
+    return answer
 # Complete all the questions.
 
 
